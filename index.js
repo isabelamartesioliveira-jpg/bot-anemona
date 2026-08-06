fd2039ab65ec59a096d91310aa10fe885989f1f1
isabelamartesioliveira-jpg <isabelamartesioliveira@gmail.com>
2026-08-06 00:45:07 -0300

    index.js

--- index.js
+++ index.js
@@ -195,12 +195,12 @@ bot.on("messageCreate", async (mensagem) => {
            const embedGuia = new EmbedBuilder()
           .setTitle("Guia de Comandos — Anêmonas")
           .setDescription(`**• Laços e Separações**
-      const embedGuia = new EmbedBuilder()
+            const embedGuia = new EmbedBuilder()
           .setTitle("Guia de Comandos — Anêmonas")
           .setDescription(`• Laços e Separações
 .casar @nome — Pede alguém em casamento.
 .divorciar @nome — Separa e divide as panelas do casal.
-.velorio @nome — Organiza um enterro de brincadeira.
+.velorio @nome — Organiza um enterro de brincadeira.`)
 
 • Fotos e Perfis
 .avatar @membro — Puxa a foto de perfil em tamanho grande.
